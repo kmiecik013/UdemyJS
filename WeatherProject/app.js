f46c8dd8a7beaf7771120d19eef22c26a45ca588
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-06-20 20:28:12 -0600

    finished mini weather app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,17 +1,55 @@
 const express = require('express')
-const app = express()
 const https = require('https');
-const port = 3000
-
-app.get('/', (req, res) => {
-
-  url = 
-  https.get(url)
+const bodyParser = require("body-parser");
+const port = 3000;
 
 
+const app = express()
 
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+  
+});
+
+app.post("/", (req, res) => {
+
+const query = req.body.cityName
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=fea93a98bf36edbc4104f8b92caa8e9b&units=metric"
+ 
+console.log(req.body)
+
+https.get(url, (response) => {
+    console.log(response.statusCode)
+
+
+    response.on("data", (data) => {
+        const weatherData = JSON.parse(data)
+        const mainTemperature = weatherData.main.temp
+        const description = weatherData.weather[0].main
+        const icon = weatherData.weather[0].icon
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+        console.log(mainTemperature);
+        console.log(description);
+
+        res.write("<p> The weather is " + description + "</p>")
+
+        res.write("<h1>The temperatur is " + (mainTemperature) + " C </h1>")
+
+        res.write("<img src=" + imageURL + ">");
+
+        res.send()
+    })
+  })
+
+
 })
+ 
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
