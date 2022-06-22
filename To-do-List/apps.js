0ccc1742f7a2b50b53fb79575e918419a630173e
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-06-22 01:16:23 -0600

    added css and formated page to be able to add items

--- To-do-List/apps.js
+++ To-do-List/apps.js
@@ -4,31 +4,64 @@ const port = 3000;
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food"];
+let workItems = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
 
-  if (currentDay === 6) {
-    day = "Saturday";
-  } else if (currentDay === 0) {
-    day = "Sunday";
-  } else if (currentDay === 1) {
-    day = "Monday";
-  } else if (currentDay === 2) {
-    day = "Tuesday";
-  } else if (currentDay === 3) {
-    day = "Wednesday";
-  } else if (currentDay === 4) {
-    day = "Thursday";
-  } else if (currentDay === 5) {
-    day = "Friday";
-  }
-  res.render("list", { kindOfDay: day });
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-us", options);
+
+  res.render("list", { listTitle: day, newListItems: items });
   console.log(currentDay, "curent day");
 });
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", (req, res) => {
+  
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-us", options);
+  res.render("about", {listTitle: day});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
