a9010a64895f95c42995bc316a90fe0c6e33637f
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-06-23 23:57:05 -0600

    finished newsletter

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const port = 3000
 const bodyParser = require("body-parser")
 const app = express();
+const http = require()
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended:true}))
@@ -16,14 +16,51 @@ app.post('/', (req, res) => {
   let lastName = req.body.lName
   let email = req.body.email
 
-  console.log(firstName, "first name")
-  console.log(lastName, "last name")
-  console.log(email, "email")
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status:"subscribed",
+        merge_fields: {
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  }
+
+  let jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/447c8cec5b"
+
+  const options = {
+    method:"POST",  
+    auth: "michaelk:e1fd623d09495d6afbee0c8bd188b689-us14"
+  }
+
+  const request = https.request(url, options, (response)=> {
+
+      console.log(response.statusCode)
+
+      response.on("data", (data) => {
+        console.log(JSON.parse(data))
+      })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+
+
+ 
 })
 
 
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//e1fd623d09495d6afbee0c8bd188b689-us14
+
