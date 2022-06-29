fce27887dbfb7aec0f427f1cd5e1b2e6c5e48920
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-06-29 00:27:10 -0600

    added first part of encryption

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require("eje")
+const ejs = require("ejs");
+const mongoose= require("mongoose")
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 
@@ -11,7 +14,70 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+const secret = "thisisourlittle secret"
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+
+
+
+const User = new mongoose.model("User", userSchema)
+
+
+app.get("/", (req, res)=> {
+  res.render("home")
+})
+
+app.get("/login", (req, res)=> {
+  res.render("login")
+})
+
+app.get("/register", (req, res)=> {
+  res.render("register")
+})
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  }) 
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets")
+    }
+  })
+})
+
+app.post("/login", (req,res)=> {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  
+
+  User.findOne({email:username}, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if(foundUser.password === password) {
+          res.render("secrets")
+        }
+      }
+    }
+  })
+})
 
 app.listen(3000, function() {
   console.log("Sever is running on port 3000.");
 });
+
+
